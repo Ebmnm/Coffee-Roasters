c953ec50fd7b831c5dbfac110bf2d6e27c373a5c
ebmnm <ebmnm@aol.com>
2021-08-08 13:25:52 -0400

    nav fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,18 +2,21 @@ import React, {useState} from 'react'
 import Logo from "../svg/Logo"
 import { Link } from 'react-router-dom';
 import "./styles/navbar.css"
+import {  Dialog } from '@reach/dialog';
 export default function Navbar() {
     const [toggleNav, setToggleNav] = useState(false)
 
     
     return (
+     
         <div className="nav-container">
            <Logo />
            <svg onClick={() => setToggleNav(!toggleNav)} className="hamberger" width="16" height="15" xmlns="http://www.w3.org/2000/svg"><path d="M14.5 12a1.5 1.5 0 010 3h-13a1.5 1.5 0 010-3h13zm0-6a1.5 1.5 0 010 3h-13a1.5 1.5 0 010-3h13zm0-6a1.5 1.5 0 010 3h-13a1.5 1.5 0 010-3h13z" fill="#333D4B" fillRule="evenodd"/></svg>
-           <div className={ toggleNav ? "show-links" : "hide-links" }>
+           <div onClick={() => setToggleNav(!toggleNav)}  className={ toggleNav ? "show-links" : "hide-links" }>
                <Link   className="nav-link" to="/Coffee-Roasters">HOME</Link>
-               <Link   className="nav-link" to="/plan">CREATE YOUR PLAN</Link>
+               <Link  className="nav-link" to="/plan">CREATE YOUR PLAN</Link>
            </div>
         </div>
+ 
     )
 }
