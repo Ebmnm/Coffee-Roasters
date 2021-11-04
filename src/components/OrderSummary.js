5622317ccfed093a3a6a28d0cf9a11635b0c89cd
mike <ebmnm77@gmail.com>
2021-11-04 14:03:44 -0400

    refactor

--- src/components/OrderSummary.js
+++ src/components/OrderSummary.js
@@ -3,63 +3,48 @@ import "./styles/orderSummary.css"
 export default function OrderSummary({selectedOption, setSelectedOption}) {
 
 function oneSelected() {
-   if(selectedOption.One === "") {
-    return  "_____" 
-   } else if(selectedOption.One === "Capsule") {
-     return " Capsules"
-   } else if(selectedOption.One === "Filter") {
-    return " Filters"
-  } else if(selectedOption.One === "Expresso") {
-    return " Expressos"
+  switch (selectedOption.One) {
+    default:  return  "_____" 
+    case "Capsule": return " Capsules"
+    case "Filter": return " Filters"
+    case "Expresso":   return " Expressos"
   }
 }
 
 function twoSelected() {
-    if(selectedOption.Two === "") {
-     return  "_____" 
-    } else if(selectedOption.Two === "Single Origin") {
-      return " Single Origin "
-    } else if(selectedOption.Two === "Decaf") {
-     return " Decaf "
-   } else if(selectedOption.Two === "Blended") {
-     return " Blended "
-   }
+  switch(selectedOption.Two) {
+    default:  return  "_____" 
+    case "Single Origin": return " Single Origin "
+    case "Decaf": return " Decaf "
+    case "Blended": return " Blended "
+  }
  }
 
  function threeSelected() {
-    if(selectedOption.Three === "") {
-     return  "_____" 
-    } else if(selectedOption.Three === "250g") {
-      return " 250g"
-    } else if(selectedOption.Three === "500g") {
-     return " 500g"
-   } else if(selectedOption.Three === "1000g") {
-     return " 1000g"
+   switch(selectedOption.Three) {
+     default: return  "_____" 
+     case "250g": return " 250g "
+     case "500g": return " 500g "
+     case "1000g": return " 1000g "
    }
  }
  function fourSelected() {
-    if(selectedOption.Four === "") {
-     return  "_____" 
-    } else if(selectedOption.Four === "Wholebean") {
-      return " Wholebean"
-    } else if(selectedOption.Four === "Filter") {
-     return " Filter"
-   } else if(selectedOption.Four === "Cafetiére") {
-     return " Cafetiére"
-   }
- }
- function fiveSelected() {
-    if(selectedOption.Five === "") {
-     return  "_____" 
-    } else if(selectedOption.Five === "Every week") {
-      return " Every week"
-    } else if(selectedOption.Five === "Every two weeks") {
-     return " Every two weeks"
-   } else if(selectedOption.Five === "Every month") {
-     return " Every month"
+   switch(selectedOption.Four) {
+     default: return  "_____"
+     case "Wholebean": return " Wholebean"
+     case "Filter": return " Filter"
+     case "Cafetiére": return " Cafetiére"
    }
  }
 
+ function fiveSelected() {
+ switch (selectedOption.Five) {
+   default:  return "_____" 
+   case "Every week": return "Every week"
+   case "Every two weeks": return "Every two weeks"
+   case "Every month": return "Every month"
+  }
+}
 
 
     return (
